59d0f68e3418873d96931a09d4b91aadc0ec19fa
Adam <154867277+adamrachman-dev@users.noreply.github.com>
2024-01-31 14:51:28 +0700

    Add playRound() to script.js

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-/*
-    This function called getComputerChoice will randomly return
-    rock, paper, or scissors.
-
-    get a random number
-    pick rock, paper, or scissors base on number
-    return the value
-*/
+// this function will generate random computer choice
 function getComputerChoice() {
+    // get a random number
     let num = Math.floor(Math.random() * 100);
+
+    // pick rock, paper, or scissors base on number
     if (num <= 33) {
-        return "Rock!";
+        return "rock";
     } else if (num > 33 && num <= 66) {
-        return "Paper!";
+        return "paper";
     } else {
-        return "Scissors!";
+        return "scissors";
     }
 }
 
-console.log(getComputerChoice());
+// this function will compare player and computer's choice
+function playRound(playerSelection, computerSelection) {
+    // make input case insensitive
+    let player = playerSelection.toLowerCase();
+
+    let comp = computerSelection;
+
+    // comparing the choices
+    if (player === comp) {
+        return "Draw! You're both choosing " + player + "!";
+    } else if (player === "rock") {
+        if (comp === "paper") {
+            return "You Lose! Paper beats Rock!"
+        } else {
+            return "You Win! Rock beats Scissors!"
+        }
+    } else if (player === "paper") {
+        if (comp === "scissors") {
+            return "You Lose! Scissors beats Paper!"
+        } else {
+            return "You Win! Paper beats Rock!"
+        }
+    } else if (player === "scissors") {
+        if (comp === "rock") {
+            return "You Lose! Rock beats Scissors!"
+        } else {
+            return "You Win! Scissors beats Paper!"
+        }
+    }
+}
